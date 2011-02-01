99cfe4754b4b2494803d48e9c8cf804061a83ec8
Anders Henja <anders@pclx006.(none)>
2011-02-01 10:51:18 +0100

    Ticket 337: Polar volume not accepted (added tests)

--- test/pytest/PgfVolumePluginTest.py
+++ test/pytest/PgfVolumePluginTest.py
@@ -1,11 +1,33 @@
+# -*- coding: latin-1 -*-
 '''
-Created on Jan 19, 2011
+Copyright (C) 2011 Swedish Meteorological and Hydrological Institute, SMHI,
 
-@author: anders
+This file is part of RAVE.
+
+RAVE is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RAVE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with RAVE.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------*/
+
+Tests the volume plugin
+
+@file
+@author Anders Henja (Swedish Meteorological and Hydrological Institute, SMHI)
+@date 2011-01-19
 '''
 import unittest
 import string
 import math
+import _raveio
 import rave_pgf_volume_plugin
 
 class PgfVolumePluginTest(unittest.TestCase):
@@ -37,6 +59,18 @@ class PgfVolumePluginTest(unittest.TestCase):
     self.assertAlmostEquals(14.0, volume.getScan(3).elangle * 180.0/math.pi, 4)
     self.assertAlmostEquals(24.0, volume.getScan(4).elangle * 180.0/math.pi, 4)
     self.assertAlmostEquals(40.0, volume.getScan(5).elangle * 180.0/math.pi, 4)
+    self.assertEquals("WMO:02092,RAD:SE41,PLC:Luleå,CMT:selul", volume.source)
+
+  def test_fix_source(self):
+    s1 = "WMO:02451,RAD:SE46,PLC:Arlanda,CMT:searl"
+    s2 = "WMO:02451,RAD:SE46,CMT:searl,PLC:Arlanda"
+    s3 = "CMT:searl,WMO:02451,RAD:SE46,PLC:Arlanda"
+    s4 = "WMO:02451,RAD:SE46,PLC:Arlanda"
+
+    self.assertEquals("WMO:02451,RAD:SE46,PLC:Arlanda,CMT:selul", rave_pgf_volume_plugin.fix_source(s1, "selul"))
+    self.assertEquals("WMO:02451,RAD:SE46,CMT:selul,PLC:Arlanda", rave_pgf_volume_plugin.fix_source(s2, "selul"))
+    self.assertEquals("CMT:selul,WMO:02451,RAD:SE46,PLC:Arlanda", rave_pgf_volume_plugin.fix_source(s3, "selul"))
+    self.assertEquals("WMO:02451,RAD:SE46,PLC:Arlanda,CMT:selul", rave_pgf_volume_plugin.fix_source(s4, "selul"))
 
 if __name__ == "__main__":
     unittest.main()
