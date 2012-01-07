215ef7f0a77c804cc270cb15f8b510715f26ae57
Anders Henja <anders@baltrad.eu>
2012-01-07 18:50:42 +0100

    Ticket 759: Patch rave to be able to communicate with dex style signing

--- Lib/BaltradFrame.py
+++ Lib/BaltradFrame.py
@@ -71,102 +71,76 @@ class BaltradFrame(object):
 
 
 def post_multipart(host, selector, fields, files):
-    content_type, body = encode_multipart_formdata(fields, files)
-    conn = httplib.HTTPConnection(host)
-    headers = {
-        "Content-Type": content_type
-    }
-    try:
-        conn.request('POST', selector, body, headers)
-        response = conn.getresponse()
-    finally:
-        conn.close()
-    return response.status, response.reason, response.read()
+  content_type, body = encode_multipart_formdata(fields, files)
+  conn = httplib.HTTPConnection(host)
+  headers = {
+    "Content-Type": content_type
+  }
+  try:
+    conn.request('POST', selector, body, headers)
+    response = conn.getresponse()
+  finally:
+    conn.close()
+  return response.status, response.reason, response.read()
 
 def encode_multipart_formdata(fields, files):
-    """
-    :param fields: a sequence of (name, value) elements for regular form
-                   fields.
-    :param files: a sequence of (name, filename, value) elements for data
-                  to be uploaded as files
-    :return: *(content_type, body)* ready for httplib.HTTP instance
-    """
-    BOUNDARY = mimetools.choose_boundary()
-    L = []
-    for (key, value) in fields:
-        L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"' % key)
-        L.append('')
-        L.append(value)
-    for (key, filename, value) in files:
-        L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-        L.append('Content-Type: %s' % get_content_type(filename))
-        L.append('')
-        L.append(value)
-    L.append('--' + BOUNDARY + '--')
+  """
+  :param fields: a sequence of (name, value) elements for regular form
+                 fields.
+  :param files: a sequence of (name, filename, value) elements for data
+                to be uploaded as files
+  :return: *(content_type, body)* ready for httplib.HTTP instance
+  """
+  BOUNDARY = mimetools.choose_boundary()
+  L = []
+  for (key, value) in fields:
+    L.append('--' + BOUNDARY)
+    L.append('Content-Disposition: form-data; name="%s"' % key)
     L.append('')
-    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
-    return content_type, "\r\n".join(L)
+    L.append(value)
+  for (key, filename, value) in files:
+    L.append('--' + BOUNDARY)
+    L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+    L.append('Content-Type: %s' % get_content_type(filename))
+    L.append('')
+    L.append(value)
+    
+  L.append('--' + BOUNDARY + '--')
+  L.append('')
+  content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+  return content_type, "\r\n".join(L)
 
 def createDsaKey(key):
-    seq = asn1.DerSequence()
-    data = "\n".join(key.strip().split("\n")[1:-1]).decode("base64")
-    seq.decode(data)
-    p, q, g, y, x = seq[1:]
-    return DSA.construct((y, g, p, q, x))
+  seq = asn1.DerSequence()
+  data = "\n".join(key.strip().split("\n")[1:-1]).decode("base64")
+  seq.decode(data)
+  p, q, g, y, x = seq[1:]
+  return DSA.construct((y, g, p, q, x))
 
 def get_content_type(filename):
-    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+  return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
 
 def inject_certificate(dex_uri, certificate):
-    frame = BaltradFrame()
-    frame.set_request_type("BF_PostCertificate")
-    frame.set_node_name(DEX_NODENAME)
-    frame.set_local_uri("http://localhost")
-    frame.set_certificate(certificate)
-    return frame.post(dex_uri)
+  frame = BaltradFrame()
+  frame.set_request_type("BF_PostCertificate")
+  frame.set_node_name(DEX_NODENAME)
+  frame.set_local_uri("http://localhost")
+  frame.set_certificate(certificate)
+  return frame.post(dex_uri)
 
 def inject_file(path, dex_uri):
-    if PRIVKEY == None:
-      raise Exception, "BaltradFrame only support encrypted communication"
+  if PRIVKEY == None:
+    raise Exception, "BaltradFrame only support encrypted communication"
 
-    frame = BaltradFrame()
-    frame.set_request_type("BF_PostDataDeliveryRequest")
-    frame.set_node_name(DEX_NODENAME)
-    frame.set_local_uri("http://localhost")
+  frame = BaltradFrame()
+  frame.set_request_type("BF_PostDataDeliveryRequest")
+  frame.set_node_name(DEX_NODENAME)
+  frame.set_local_uri("http://localhost")
 
-    key = createDsaKey(PRIVKEY)
-    frame.sign(key)
-    frame.set_payload_file(path)
-    return frame.post(dex_uri)
-
-
-#PRIVKEY2 = """-----BEGIN DSA PRIVATE KEY-----
-#MIIBuwIBAAKBgQD9f1OBHXUSKVLfSpwu7OTn9hG3UjzvRADDHj+AtlEmaUVdQCJR
-#+1k9jVj6v8X1ujD2y5tVbNeBO4AdNG/yZmC3a5lQpaSfn+gEexAiwk+7qdf+t8Yb
-#+DtX58aophUPBPuD9tPFHsMCNVQTWhaRMvZ1864rYdcq7/IiAxmd0UgBxwIVAJdg
-#UI8VIwvMspK5gqLrhAvwWBz1AoGBAPfhoIXWmz3ey7yrXDa4V7l5lK+7+jrqgvlX
-#TAs9B4JnUVlXjrrUWU/mcQcQgYC0SRZxI+hMKBYTt88JMozIpuE8FnqLVHyNKOCj
-#rh4rs6Z1kW6jfwv6ITVi8ftiegEkO8yk8b6oUZCJqIPf4VrlnwaSi2ZegHtVJWQB
-#TDv+z0kqAoGAafFTFzcMqfCwbf0Q/BnmCsk6PRU3Efy/iz2D4s8WHRDM9lg2drBb
-#yCIpPLJkmzi+qFFgSMMcKKYVMNbEs3BdyjsrnjW3zXc6zZWzDYY9nraoVVXPJ7Ik
-#N3u2HwtB1m/afHED8mcBf2OpcfJ6GWyeB1kh56BB2b3jSW+hj6Gsw7ACFD+SquSp
-#SGNQVh+cc+gW72maAOR3
-#-----END DSA PRIVATE KEY-----"""
-
-#PRIVKEY = """-----BEGIN DSA PRIVATE KEY-----
-#MIIBvAIBAAKBgQD9f1OBHXUSKVLfSpwu7OTn9hG3UjzvRADDHj+AtlEmaUVdQCJR
-#+1k9jVj6v8X1ujD2y5tVbNeBO4AdNG/yZmC3a5lQpaSfn+gEexAiwk+7qdf+t8Yb
-#+DtX58aophUPBPuD9tPFHsMCNVQTWhaRMvZ1864rYdcq7/IiAxmd0UgBxwIVAJdg
-#UI8VIwvMspK5gqLrhAvwWBz1AoGBAPfhoIXWmz3ey7yrXDa4V7l5lK+7+jrqgvlX
-#TAs9B4JnUVlXjrrUWU/mcQcQgYC0SRZxI+hMKBYTt88JMozIpuE8FnqLVHyNKOCj
-#rh4rs6Z1kW6jfwv6ITVi8ftiegEkO8yk8b6oUZCJqIPf4VrlnwaSi2ZegHtVJWQB
-#TDv+z0kqAoGBANBoawdam6WE3jAebLhUQ7XP5aD9EimPTAjtt+PdTWx1zqH4hKFr
-#ygmL/8nh/BsQOiLKo/XOUmmafuby8P5Uke30t/BphU07fVepan1oQicYdlNVemNX
-#AIGdB3a58AJ8XAkx9ayDRDQ9w2VDMd2R75qF1gwjxPYPSL6XO39E1FQVAhQXiFyh
-#AmWPEVj0t8HEp4nAePTX2w==
-#-----END DSA PRIVATE KEY-----"""
+  key = createDsaKey(PRIVKEY)
+  frame.sign(key)
+  frame.set_payload_file(path)
+  return frame.post(dex_uri)
 
 if __name__ == "__main__":
   dex_url = DEX_SPOE
@@ -183,10 +157,8 @@ if __name__ == "__main__":
     print "url is optional, if not specified, then it will default to DEX_SPOE in rave_defines"
     print ""
     sys.exit(0)
-    #dex_url = "http://localhost:9001/BaltradDex/dispatch.htm"
-    #node_name = "test_node"
-    #    print inject_certificate(dex_url, node_name, sys.argv[1])
-    if sys.argv[1] == "certificate":
-      print inject_certificate(dex_url, filename)
-    else:
-      print inject_file(filename, dex_url)
+    
+  if sys.argv[1] == "certificate":
+    print inject_certificate(dex_url, filename)
+  else:
+    print inject_file(filename, dex_url)
