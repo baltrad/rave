3b867be2763320d92cdecb5f65d892049a0ea195
Daniel Michelson <daniel.michelson@canada.ca>
2015-12-17 16:50:58 +0000

    Ticket 344: rave_scansun_quality_plugin.py

---
+++ Lib/rave_scansun_quality_plugin.py
@@ -0,0 +1,61 @@
+'''
+Copyright (C) 2015 The Crown (i.e. Her Majesty the Queen in Right of Canada)
+
+This file is part of RAVE.
+
+RAVE is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RAVE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with RAVE.  If not, see <http://www.gnu.org/licenses/>.
+'''
+##
+# A quality plugin with scansun support 
+
+## 
+# @file
+# @author Daniel Michelson, Environment and Climate Change Canada
+# @date 2015-12-16
+
+from rave_quality_plugin import rave_quality_plugin
+import _polarscan, _polarvolume
+import rave_pgf_logger
+from rave_pgf_scansun_plugin import writeHits
+logger = rave_pgf_logger.rave_pgf_syslog_client()
+
+    
+class scansun_quality_plugin(rave_quality_plugin):
+  ##
+  # Default constructor
+  def __init__(self):
+    super(scansun_quality_plugin, self).__init__()
+  
+  ##
+  # @return a list containing the string nl.knmi.scansun
+  # This is just a placeholder. String won't be used in this case.
+  def getQualityFields(self):
+    return ["nl.knmi.scansun"]
+  
+  ##
+  # @param obj: A RAVE object that should be processed.
+  # @param reprocess_quality_flag: If quality flag should be reprocessed or not
+  # @param arguments: Not used
+  # @return: The modified object if this quality plugin has performed changes 
+  # to the object.
+  def process(self, obj, reprocess_quality_flag=True, arguments=None):
+    try:
+      import _scansun
+      if reprocess_quality_flag:  # scansun doesn't modify the payload, so there's no indicator that scansun has been run before.
+        source, hits = _scansun.scansunFromObject(obj)
+        if len(hits) > 0:
+          writeHits(source, hits)
+    except:
+      logger.exception("Failure during scansun processing")
+    return obj
--- test/pytest/RaveScansun.py
+++ test/pytest/RaveScansun.py
@@ -3,13 +3,23 @@ Created on 20 January 2011
 
 @author: Daniel Michelson
 '''
-import unittest
+import os, unittest
 import _scansun
+import _raveio
 from numpy import nan
+import rave_pgf_scansun_plugin, odim_source
+from rave_defines import UTF8
 
 class RaveScansun(unittest.TestCase):
     # KNMI PVOL from Den Helder with a nice sun hit
     KNMI_TESTFILE="fixtures/KNMI-PVOL-Den_Helder.h5"
+    # The following validation values are:
+    # Date    Time   Elevatn Azimuth ElevSun AzimSun   N  dBSunFlux   SunMean SunStdd   ZdrMean ZdrStdd Refl  ZDR
+    VALID = ('RAD:NL51;PLC:nldhl', [(20110111, 75022.0, 0.30000001447042407, 126.0, 
+                                     -0.77586040298515435, 126.84009882306225, 
+                                     98, 12.706416334302883, 
+                                     -113.2001649139023, 0.78867323519645993, 
+                                     nan, nan, 'DBZH', 'NA')])
 
     def setUp(self):
         pass
@@ -34,30 +44,53 @@ class RaveScansun(unittest.TestCase):
 
 
     def testScansun(self):
-        # The following validation values are:
-        # Date    Time   Elevatn Azimuth ElevSun AzimSun   N  dBSunFlux   SunMean SunStdd   ZdrMean ZdrStdd Refl  ZDR
-        valid = ('RAD:NL51;PLC:nldhl', [(20110111, 75022.0, 0.30000001447042407, 126.0, 
-                                         -0.77586040298515435, 126.84009882306225, 
-                                         98, 12.706416334302883, 
-                                         -113.2001649139023, 0.78867323519645993, 
-                                         nan, nan, 'DBZH', 'NA')])
         result = _scansun.scansun(self.KNMI_TESTFILE)
-        self.assertEquals(valid[0][0], result[0][0])
-        self.assertEquals(valid[1][0][0], result[1][0][0])
-        self.assertEquals(valid[1][0][1], result[1][0][1])
-        self.assertAlmostEquals(valid[1][0][2], result[1][0][2], 5)
-        self.assertAlmostEquals(valid[1][0][3], result[1][0][3], 5)
-        self.assertAlmostEquals(valid[1][0][4], result[1][0][4], 5)
-        self.assertAlmostEquals(valid[1][0][5], result[1][0][5], 5)
-        self.assertEquals(valid[1][0][6], result[1][0][6])
-        self.assertAlmostEquals(valid[1][0][7], result[1][0][7], 5)
-        self.assertAlmostEquals(valid[1][0][8], result[1][0][8], 5)
-        self.assertAlmostEquals(valid[1][0][9], result[1][0][9], 5)
-        #self.assertEquals(str(valid[1][0][10]), str(result[1][0][10]))
-        #self.assertEquals(str(valid[1][0][11]), str(result[1][0][11]))
-        self.assertEquals(valid[1][0][12], result[1][0][12])
-        self.assertEquals(valid[1][0][13], result[1][0][13])
-	#self.assertEquals(valid, _scansun.scansun(self.KNMI_TESTFILE))
+        self.assertEquals(self.VALID[0][0], result[0][0])
+        self.assertEquals(self.VALID[1][0][0], result[1][0][0])
+        self.assertEquals(self.VALID[1][0][1], result[1][0][1])
+        self.assertAlmostEquals(self.VALID[1][0][2], result[1][0][2], 5)
+        self.assertAlmostEquals(self.VALID[1][0][3], result[1][0][3], 5)
+        self.assertAlmostEquals(self.VALID[1][0][4], result[1][0][4], 5)
+        self.assertAlmostEquals(self.VALID[1][0][5], result[1][0][5], 5)
+        self.assertEquals(self.VALID[1][0][6], result[1][0][6])
+        self.assertAlmostEquals(self.VALID[1][0][7], result[1][0][7], 5)
+        self.assertAlmostEquals(self.VALID[1][0][8], result[1][0][8], 5)
+        self.assertAlmostEquals(self.VALID[1][0][9], result[1][0][9], 5)
+        self.assertEquals(self.VALID[1][0][12], result[1][0][12])
+        self.assertEquals(self.VALID[1][0][13], result[1][0][13])
+
+
+    def testScansunFromObject(self):
+        # Replicate the above test, but using in-memory processing.
+        obj = _raveio.open(self.KNMI_TESTFILE).object
+        result = _scansun.scansunFromObject(obj)
+        self.assertEquals(self.VALID[0][0], result[0][0])
+        self.assertEquals(self.VALID[1][0][0], result[1][0][0])
+        self.assertEquals(self.VALID[1][0][1], result[1][0][1])
+        self.assertAlmostEquals(self.VALID[1][0][2], result[1][0][2], 5)
+        self.assertAlmostEquals(self.VALID[1][0][3], result[1][0][3], 5)
+        self.assertAlmostEquals(self.VALID[1][0][4], result[1][0][4], 5)
+        self.assertAlmostEquals(self.VALID[1][0][5], result[1][0][5], 5)
+        self.assertEquals(self.VALID[1][0][6], result[1][0][6])
+        self.assertAlmostEquals(self.VALID[1][0][7], result[1][0][7], 5)
+        self.assertAlmostEquals(self.VALID[1][0][8], result[1][0][8], 5)
+        self.assertAlmostEquals(self.VALID[1][0][9], result[1][0][9], 5)
+        self.assertEquals(self.VALID[1][0][12], result[1][0][12])
+        self.assertEquals(self.VALID[1][0][13], result[1][0][13])
+
+
+    def testWriteHits(self):
+        source = odim_source.SOURCE['nldhl'].encode(UTF8)  # Non-compliant ODIM source in fixture...
+        hits = self.VALID[1]
+        rave_pgf_scansun_plugin.RAVEETC = os.getcwd()+'/fixtures'
+        fstr = '%s/scansun/nldhl.scansun' % rave_pgf_scansun_plugin.RAVEETC
+        rave_pgf_scansun_plugin.writeHits(source, hits)
+        fd = open(fstr)
+        content = fd.read()
+        fd.close()
+        self.assertEqual(content, '#Date    Time        Elevatn Azimuth   ElevSun   AzimSun    N  dBSunFlux   SunMean SunStdd   ZdrMean ZdrStdd  Refl  ZDR\n20110111 075022.000    0.300  126.00   -0.7759  126.8401      98      12.71   -113.20   0.789       nan     nan  DBZH   NA\n')
+        os.remove(fstr)
+        os.rmdir(os.path.split(fstr)[0])
 
 
 if __name__ == "__main__":
