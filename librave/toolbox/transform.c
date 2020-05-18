4e26a8bb072729289906a0d43ab15de28c5f5e2a
Anders Henja <anders@baltrad.eu>
2020-05-18 09:41:03 +0200

    Ticket 850: Combine tiles might cause an empty lines between various tiles.

--- librave/toolbox/transform.c
+++ librave/toolbox/transform.c
@@ -639,9 +639,8 @@ static int TransformInternal_addTileToParameter(Transform_t* self, Cartesian_t* 
   sysize = Cartesian_getYSize(source);
 
   /* A tile should have some sort of offset in relation to the target */
-  xoffset = (long)rint((sllX - tllX) / xscale);
-  yoffset = (long)rint((turY - surY) / yscale);
-
+  xoffset = (long)((sllX - tllX) / xscale); /*previously rint but that will ocassionally cause gaps between tiles*/
+  yoffset = (long)((turY - surY) / yscale); /*and it's better to always floor it*/
   for (x = 0; x < sxsize; x++) {
     for (y = 0; y < sysize; y++) {
       double v = 0.0;
