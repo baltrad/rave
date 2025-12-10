f3681df7352c724167a1f04a880c112059d700f4
Anders Henja <anders@baltrad.eu>
2025-12-10 18:00:00 +0100

    Corrected bug in rave_gra_coefficient.identifier upgrade

--- Lib/ravemigrate/versions/004_add_id_to_rave_gra_coefficients.py
+++ Lib/ravemigrate/versions/004_add_id_to_rave_gra_coefficients.py
@@ -6,7 +6,7 @@ import contextlib
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
     rave_gra_coefficient = Table('rave_gra_coefficient', meta, autoload=True)
-    identifier_column = Column("identifier", Text, nullable=False)
+    identifier_column = Column("identifier", Text, nullable=True)
     identifier_column.create(rave_gra_coefficient)
 
     old_key = constraint.PrimaryKeyConstraint("area", "date", "time", table=rave_gra_coefficient)
@@ -15,19 +15,22 @@ def upgrade(migrate_engine):
     for primary_key in rave_gra_coefficient.primary_key:
         primary_key.primary_key = False
 
-    new_key = constraint.PrimaryKeyConstraint("identifier", "area", "date", "time", table=rave_gra_coefficient)
-    new_key.create()
-
     with contextlib.closing(migrate_engine.connect()) as conn:
         conn.execute("update rave_gra_coefficient set area = '' where area = 'n/a'")
+        conn.execute("update rave_gra_coefficient set identifier = ''")
+    rave_gra_coefficient.c.identifier.alter(nullable=False)
+
+    new_key = constraint.PrimaryKeyConstraint("identifier", "area", "date", "time", table=rave_gra_coefficient)
+    new_key.create()
 
 def downgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
     rave_gra_coefficient = Table('rave_gra_coefficient', meta, autoload=True)
-    rave_gra_coefficient.c.identifier.drop()
 
     new_key = constraint.PrimaryKeyConstraint("identifier", "area", "date", "time", table=rave_gra_coefficient)
     new_key.drop()
+
+    rave_gra_coefficient.c.identifier.drop()
 
     for primary_key in rave_gra_coefficient.primary_key:
         primary_key.primary_key = False
